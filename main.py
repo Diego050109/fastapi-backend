69c1d9abcfaa2f621d2d700467e6c3dadd1a03f6
Diego050109 <lemadiego597@gmail.com>
2025-12-12 12:37:04 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+import os
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@db:5432/postgres"
+)
+
+engine = create_engine(DATABASE_URL)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"mensaje": "Hola Mundo desde FastAPI 🚀"}
+
+@app.get("/db-check")
+def db_check():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT 1"))
+        return {"db": result.scalar()}
